8d1f84719f2f4508534954af26e7f5a2ab66fcd3
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-31 10:18:50 +0100

    Update ej3b2.py

--- 3b/ej3b2.py
+++ 3b/ej3b2.py
@@ -42,11 +42,11 @@ PRINT_LOGS: bool = True
 
 class LogMethodCalls(object):
     def __init__(self, print_logs: bool = True) -> None:
-        self.print_logs: bool = 
+        self.print_logs: bool = True
 
     def __call__(self, func: Callable) -> Callable:
         def wrapped(*args: Any, **kwargs: Any) -> Any:
-            if :
+            if self.print_logs:
                 args_repr: list[str] = [
                     repr(a) for a in args
                 ]  # List of argument representations
@@ -55,7 +55,7 @@ class LogMethodCalls(object):
                 ]  # List of key=value representations
                 signature: str = ", ".join(args_repr + kwargs_repr)
                 logging.info("Calling %s (%s)", func.__name__, signature)
-            return 
+            return func(*args, **kwargs)
 
         return wrapped
 
@@ -65,12 +65,12 @@ class LogMethodCalls(object):
 
 @LogMethodCalls(print_logs=PRINT_LOGS)
 def load_csv(filename: str) -> pd.DataFrame:
-    return pd.read_csv()
+    return pd.read_csv(filename)
 
 
 @LogMethodCalls(print_logs=PRINT_LOGS)
 def load_and_describe_csv(filename: str) -> pd.DataFrame:
-    dataframe: pd.DataFrame = pd.read_csv()
+    dataframe: pd.DataFrame = pd.read_csv(filename)
     return dataframe.describe()
 
 
@@ -82,3 +82,10 @@ def load_and_describe_csv(filename: str) -> pd.DataFrame:
 #     print(dataframe_credit.head(1))
 #     description = load_and_describe_csv(FILENAME_PATH)
 #     print(description)
+if __name__ == "__main__":
+    path_parent = Path(__file__).parent
+    FILENAME_PATH = path_parent / 'data/german_credit_data.csv'
+    dataframe_credit = load_csv(FILENAME_PATH)
+    print(dataframe_credit.head(1))
+    description = load_and_describe_csv(FILENAME_PATH)
+    print(description)
