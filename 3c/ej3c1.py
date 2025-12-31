d95b14316fbbfecf5a6a143338e38fa09780ab35
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-31 10:20:51 +0100

    Update ej3c1.py

--- 3c/ej3c1.py
+++ 3c/ej3c1.py
@@ -26,11 +26,10 @@ Ejemplo de Salida Esperada:
     Temperatures in Fahrenheit: [32.0, 50.0, 68.0, 95.0, 113.0, 140.0]
 """
 
-temperatures_celsius = [54, 84, 38, 104, 101, 107, 55, 1, 38, 31, 109, 6, 91, 46, 16, 28, 74, 102, 20, 39]
-filter_temperatures_celsius = list(filter(lambda x:))
-convert_to_fahrenheit = lambda celsius: 
-temperatures_fahrenheit = list(map())
+filter_temperatures_celsius = list(filter(lambda x: x < 60, temperatures_celsius))
+convert_to_fahrenheit = lambda celsius: (celsius * 9 / 5) + 32
+temperatures_fahrenheit = list(map(convert_to_fahrenheit, filter_temperatures_celsius))
 
-# Para probar el código, descomenta las siguientes líneas
-# print("Temperatures in Celsius:", temperatures_celsius)
-# print("Temperatures in Fahrenheit:", temperatures_fahrenheit)
+print("Temperatures in Celsius:", temperatures_celsius)
+print("Temperatures in Fahrenheit:", temperatures_fahrenheit)
+
